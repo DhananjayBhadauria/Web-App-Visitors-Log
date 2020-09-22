75aabe80c6340bfc0120f9f888d3968804e33343
DHANANJAY SINGH BHADAURIA <40880864+DhananjayBhadauria@users.noreply.github.com>
2020-09-22 13:15:45 +0530

    kdfj;

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
       user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="profile")
       fullName = models.CharField(max_length=60)
+      photo = models.ImageField(upload_to="ClientProfilePics/", null=True, blank=True)
 
       def __str__(self):
             return str(self.user)
